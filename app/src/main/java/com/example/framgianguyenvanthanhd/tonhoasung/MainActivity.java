4835092660c960abfcbbd47bfddcf6a6ec94cf39
oNguyenVanThanhD <nguyen.van.thanhd@framgia.com>
2018-07-20 13:48:47 +0700

    init

---
+++ app/src/main/java/com/example/framgianguyenvanthanhd/tonhoasung/MainActivity.java
@@ -0,0 +1,13 @@
+ntpackage com.example.framgianguyenvanthanhd.tonhoasung;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
